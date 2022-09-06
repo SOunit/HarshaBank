1663b0c1d35cab56a8d3f1960afc00273d72fb4f
SOunit <workbookwork7@gmail.com>
2022-09-05 23:11:43 -0700

    Add project files.

---
+++ HarshaBank.Presentation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HarshaBank.Presentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************** Harsha Bank ****************");
+            Console.WriteLine("::Login Page::");
+
+            string userName = null, password = null;
+            Console.WriteLine("");
+
+            Console.Write("UserName:");
+            userName = Console.ReadLine();
+
+            if (userName != "")
+            {
+                Console.Write("Password:");
+                password = Console.ReadLine();
+
+            }
+
+            if (userName == "system" && password == "manager")
+            {
+                Console.WriteLine("TO DO: Main menu here");
+            }
+
+            Console.WriteLine("Thank you! Visit Again");
+        }
+    }
+}
