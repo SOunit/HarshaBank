42bcfbdc0da625573f791dd21041ff4cd49e6174
SOunit <workbookwork7@gmail.com>
2022-09-05 23:34:01 -0700

    add menu

--- HarshaBank.Presentation/Program.cs
+++ HarshaBank.Presentation/Program.cs
@@ -41,8 +41,10 @@ namespace HarshaBank.Presentation
                     switch (mainMenuChoice)
                     {
                         case 1:
+                            CustomersMenu();
                             break;
                         case 2:
+                            AccountsMenu();
                             break;
                         case 3:
                             break;
@@ -64,6 +66,42 @@ namespace HarshaBank.Presentation
             }
 
             Console.WriteLine("Thank you! Visit Again");
+        }
+
+        static void CustomersMenu()
+        {
+            int customerMenuChoice = -1;
+
+            do
+            {
+                Console.WriteLine("\n::Customer Menu::");
+                Console.WriteLine("1. Add Customer");
+                Console.WriteLine("2. Delete Customer");
+                Console.WriteLine("3. Update Customer");
+                Console.WriteLine("4. View Customers");
+                Console.WriteLine("0. Back to Main Menu");
+
+                customerMenuChoice = Convert.ToInt32(Console.ReadLine());
+
+            } while (customerMenuChoice != 0);
+        }
+
+        static void AccountsMenu()
+        {
+            int accountsMenuChoice = -1;
+
+            do
+            {
+                Console.WriteLine("\n::Accounts Menu::");
+                Console.WriteLine("1. Add Account");
+                Console.WriteLine("2. Delete Account");
+                Console.WriteLine("3. Update Account");
+                Console.WriteLine("4. View Accounts");
+                Console.WriteLine("0. Back to Main Menu");
+
+                accountsMenuChoice = Convert.ToInt32(Console.ReadLine());
+
+            } while (accountsMenuChoice != 0);
         }
     }
 }
